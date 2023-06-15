b4aacee58d3e8809645410e167a3cf9438ccafcd
Olof <olof.hag123@gmail.com>
2023-06-15 09:29:40 +0200

    add final code

--- script.js
+++ script.js
@@ -9,27 +9,19 @@ const firebaseConfig = {
   messagingSenderId: "1089764169541",
   appId: "1:1089764169541:web:824a50162eab726448502c",
   measurementId: "G-ZHC77BMQQE",
-};
+};``
 firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database();
+let database = firebase.database();
 // Initialize Firebase
+let ref = database.ref("todos");
 
-const ref = database.ref("todos");
-function writeUserData(title, description, endDate) {
-  const newTodo = ref.push();
-  //Lägg till .flash funktion för att spara senaste tilllägget
-  newTodo.set({
-    title: title,
-    description: description,
-    endDate: endDate,
-    done: false,
-  });
-}
 
 document.addEventListener("DOMContentLoaded", function () {
   let todoBtn = document.getElementById("add-todo");
   todoBtn.addEventListener("click", addTodo);
+
+  loadMarkedTodoIds(); // återställa markerade todos från localStorage
 
   displayTodoList();
 });
@@ -40,61 +32,11 @@ function addTodo() {
   let date = document.getElementById("todo-date").value;
 
   if (titleInput === "" || description === "" || date === "") {
+    alert("Var vänlig fyll i alla fält");
     console.log("Var vänlig fyll i alla fält");
     return;
   }
-}
-/*
-function displayTodoList() {
-  //console.log(params);
-  let displayList = document.querySelector(".display-todo");
-  // Hämta data från en specifik sökväg i databasen
-  displayList.innerHTML = "";
-  ref.once(
-    "value",
-    function (snapshot) {
-      Object.keys(snapshot.val()).forEach((key) => {
-        let data = snapshot.val()[key];
-        console.log(data);
 
-        // for (let key in data) {
-        displayList.innerHTML += `<div id="${key}" class="display-things">
-          <h4>Title:</h4> 
-          <p class="header-display"> ${data["title"]} </p> 
-          <h4>Description: </h4>   
-          <p class="header-display">${data["description"]}</p>
-          <h4>Date: </h4> 
-          <p class="header-display">${data["endDate"]}</p>
-          <button class="done-button">Done</button>
-          <button class="delete-button">Delete</button>
-          <button class="update-button">Update</button>
-          </div>`;
-      });
-      // }
-
-      // ===== Delete-knappen/ deleteTodo =====
-
-      let deleteButtons = document.getElementsByClassName("delete-button");
-      for (let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener("click", deleteTodo);
-      }
-
-      // ===== Done-knappen/ done =====
-
-      let doneButtons = document.getElementsByClassName("done-button");
-      for (let i = 0; i < doneButtons.length; i++) {
-        doneButtons[i].addEventListener("click", doneTodo);
-      }
-
-      // ===== Update-knappen =====
-
-      let updateButtons = document.getElementsByClassName("update-button");
-      for (let i = 0; i < updateButtons.length; i++) {
-        updateButtons[i].addEventListener("click", updateTodo);
-      }
-    
-
-}
   let newTodo = ref.push();
   let todoId = newTodo.key;
 
@@ -110,14 +52,10 @@ function displayTodoList() {
   document.getElementById("todo-title").value = "";
   document.getElementById("todo-desc").value = "";
   document.getElementById("todo-date").value = "";
-
-
 }
-*/
+
 function displayTodoList() {
   let displayList = document.querySelector(".display-todo");
-
-  //parentElement.remove();
 
   ref.on("value", function (snapshot) {
     let data = snapshot.val();
@@ -132,7 +70,6 @@ function displayTodoList() {
       let todoContent = document.createElement("div");
       todoContent.classList.add("todoDiv");
       todoContent.id = key;
-
       let titleHeading = document.createElement("h4");
       titleHeading.innerText = "Title:";
       let titleParagraph = document.createElement("p");
@@ -151,7 +88,7 @@ function displayTodoList() {
       dateParagraph.classList.add("header-display");
       dateParagraph.innerText = todo.endDate;
 
-      // ===== testar att lägga till varning när slutdatum närnar sig =====
+      // ===== Varning när slutdatum närnar sig =====
 
       let currentDate = new Date();
       let endDate = new Date(todo.endDate);
@@ -165,6 +102,8 @@ function displayTodoList() {
         warningMessage.style.color = "orange";
         dateParagraph.appendChild(warningMessage);
       }
+
+      // ===== ===== =====
 
       let deleteButton = document.createElement("button");
       deleteButton.classList.add("delete-button");
@@ -182,9 +121,11 @@ function displayTodoList() {
       let updateButton = document.createElement("button");
       updateButton.classList.add("update-button");
       updateButton.innerText = "Update";
+
       updateButton.addEventListener("click", updateTodo);
-      //   updateTodo(key);
-      // });
+      /* updateButton.addEventListener("click", function () {
+        updateTodo(key);
+      }); */
 
       todoContent.appendChild(titleHeading);
       todoContent.appendChild(titleParagraph);
@@ -199,22 +140,76 @@ function displayTodoList() {
       todoElement.appendChild(todoContent);
       displayList.appendChild(todoElement);
     }
+
+    loadMarkedTodoIds(); // återställa markerade todos från localStorage
+    updateTodoDisplay(); // uppdatera visningen av markerade todos
   });
 }
 
+
+
 function deleteTodo(todoId) {
+  // ta bort todo från databasen
   database.ref("todos/" + todoId).remove();
+
+  // kontrollera om todo-id:t finns i markedTodoIds arrayen
+  const index = markedTodoIds.indexOf(todoId);
+  if (index >= 0) {
+    // ta bort id:t från markedTodoIds arrayen
+    markedTodoIds.splice(index, 1);
+
+    // uppdatera localStorage med den uppdaterade arrayen
+    localStorage.setItem("markedTodoIds", JSON.stringify(markedTodoIds));
+  }
 }
+
+let markedTodoIds = [];
 
 function doneTodo() {
   let parentElement = this.parentElement;
-  let todoId = parentElement
-    .querySelector(".done-checkbox")
-    .getAttribute("data-todo-id");
+  let todoId = parentElement.querySelector(".done-checkbox").getAttribute("data-todo-id");
   console.log(todoId);
+
+  if (this.checked) {
+    database.ref("todos/" + todoId).update({ done: true });
+    console.log("Uppgiften är markerad som klar");
+    markedTodoIds.push(todoId); // lägg till id i markerade todos
+  } else {
+    database.ref("todos/" + todoId).update({ done: false });
+    console.log("Uppgiften är inte markerad som klar");
+    const index = markedTodoIds.indexOf(todoId);
+    if (index !== -1) {
+      markedTodoIds.splice(index, 1); // ta bort id från markerade todos
+    }
+  }
+
+  updateTodoDisplay(); // uppdatera visningen av markerade todos
+  saveMarkedTodoIds(); // spara i localStorage
 }
 
-// ===== "update todo" function =====
+function updateTodoDisplay() {
+  const checkboxes = document.querySelectorAll(".done-checkbox");
+
+  checkboxes.forEach((checkbox) => {
+    const todoId = checkbox.getAttribute("data-todo-id");
+    checkbox.checked = markedTodoIds.includes(todoId); // markera checkboxen om id finns i markerade todos
+  });
+}
+
+function saveMarkedTodoIds() {
+  localStorage.setItem("markedTodoIds", JSON.stringify(markedTodoIds));
+}
+
+function loadMarkedTodoIds() {
+  const savedMarkedTodoIds = localStorage.getItem("markedTodoIds");
+  if (savedMarkedTodoIds) {
+    markedTodoIds = JSON.parse(savedMarkedTodoIds);
+    updateTodoDisplay();
+
+  }
+}
+
+
 function updateTodo(event) {
   const parent = event.target.parentNode;
   const docId = parent.id;
@@ -230,9 +225,6 @@ function updateTodo(event) {
     `#${docId} p:nth-of-type(3)`
   ).innerText;
 
-  //const currentTitle = document.querySelector(`#${docId} p`)[0].innerText;
-  //const currentDescription = document.querySelector(`#${docId} p`)[1].innerText;
-  //const currentEndDate = document.querySelector(`#${docId} p`)[2].innerText;
   //Modal
   const modal = document.getElementById("updateModal");
   modal.style.display = "block";
@@ -264,8 +256,6 @@ function updateTodo(event) {
     updateRef.once("value").then((snapshot) => {
       const data = snapshot.val();
 
-      //let newtitle = prompt("Enter new title: ", data.title);
-
       updateRef
         .update({
           title: titleInput.value,
@@ -283,17 +273,3 @@ function updateTodo(event) {
     });
   });
 }
-
-
-
-displayTodoList();
-
-
-if (this.checked) {
-  database.ref("todos/" + todoId).update({ done: true });
-  console.log("Uppgiften är markerad som klar");
-} else {
-  database.ref("todos/" + todoId).update({ done: false });
-  console.log("Uppgiften är inte markerad som klar");
-}
-
